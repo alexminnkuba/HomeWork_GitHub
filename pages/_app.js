67f91d7675cd1710281050e82e18763c047d3ecc
Alex <aleksei.minn2014@gmail.com>
2025-09-17 16:31:49 +0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import "@/styles/globals.css";
+import "../pages/styles.css";
 
-export default function App({ Component, pageProps }) {
+function Users({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export default Users;
